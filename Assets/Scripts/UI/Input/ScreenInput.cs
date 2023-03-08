ec49e367c71bc11043015f577090a58a500fac11
Denis-Mozgovyy <denismozgovoj07@gmail.com>
2023-03-08 14:36:46 +0200

    [System] Mobile input setup

--- Assets/Scripts/UI/Input/ScreenInput.cs
+++ Assets/Scripts/UI/Input/ScreenInput.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using Zenject;
 
-public class ScreenInput : MonoBehaviour
+public class ScreenInput: MonoBehaviour
 {
+    public Joystick Joystick { get { return _fixedJoystick; } }
+    [SerializeField] private Joystick _fixedJoystick;
+
     [Inject] private InputType _inputType;
+    [Inject] private IPlayerInput _playerInput;
 
     private void Awake()
     {
         if(_inputType == InputType.Devices)
         {
             Destroy(gameObject);
+        }
+        else
+        {
+           ((PlayerMobileInput)_playerInput).Setup(this);
         }
     }
 }
