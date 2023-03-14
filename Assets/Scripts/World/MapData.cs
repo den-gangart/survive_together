8b4f965236275acfebc9df09fdee8fb5393acea8
Denis-Mozgovyy <denismozgovoj07@gmail.com>
2023-03-14 21:25:43 +0200

    [System] Map data update

--- Assets/Scripts/World/MapData.cs
+++ Assets/Scripts/World/MapData.cs
@@ -8,66 +8,16 @@ public class MapData : ScriptableObject
     [Space]
     [Header("===== Json Data Field =====")]
     public string jsonDataMap;
-    public SavebleMapData _mapData;
+    [SerializeField] private MapConstructor _map;
    
     public void SaveAndConvertToJsonData(MapConstructor map)
     {
-        var mapTilesArrObj = map.tiles;
-
-        _mapData = new SavebleMapData()
-        {
-            mapHeight = map.rows,
-            mapWidth = map.columns,
-            tiles = new TileData[mapTilesArrObj.Length],
-        };
-
-
-        for (var i= 0; i < mapTilesArrObj.Length; i++)
-        {
-            _mapData.tiles[i] = new TileData()
-            {
-                index = mapTilesArrObj[i].autotileID,
-                isUsed = false,
-            };
-        }
-
-        jsonDataMap = JsonUtility.ToJson(_mapData);
+        jsonDataMap = JsonUtility.ToJson(map);
     }
 
     public MapConstructor LoadFromJson()
     {
-        _mapData = JsonUtility.FromJson<SavebleMapData>(jsonDataMap);
-
-        MapConstructor mapConstructor = new MapConstructor()
-        {
-            columns = _mapData.mapWidth,
-            rows = _mapData.mapHeight,
-            tiles = new MapElement[_mapData.tiles.Length],
-        };
-
-        for (var i = 0; i < _mapData.tiles.Length; i++)
-        {
-            mapConstructor.tiles[i] = new MapElement()
-            {
-                autotileID = _mapData.tiles[i].index,
-            };
-        }
-
+        MapConstructor mapConstructor = JsonUtility.FromJson<MapConstructor>(jsonDataMap);;
         return mapConstructor;
     }
-}
-
-[System.Serializable]
-public class SavebleMapData
-{
-    public int mapWidth;
-    public int mapHeight;
-    public TileData[] tiles;
-}
-
-[System.Serializable]
-public class TileData
-{
-    public int index;
-    public bool isUsed;
 }
